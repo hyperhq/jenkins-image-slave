5ddbe93f501685d045aa65a94c11aeff390a863d
Jimmy Xu <xjimmyshcn@gmail.com>
2016-12-14 23:25:22 +0800

    [master] add hyperhq/jenkins-slave-hyper:ubuntu1604

---
+++ jenkins-slave-hyper/ubuntu1604/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+repo="hyperhq/jenkins-slave-hyper"
+tag="ubuntu1604"
+image=${repo}:${tag}
+
+# DOCKER0=$(ifconfig | grep docker0 -A1 | grep "inet " | awk '{print $2}')
+# PROXY="http://${DOCKER0}:8118"
+
+function build(){
+    echo "starting build..."
+    echo "=============================================================="
+    CMD="docker build --build-arg HTTP_PROXY=${PROXY} --build-arg HTTPS_PROXY=${PROXY} -t ${image} ."
+    echo "CMD: [ ${CMD} ]"
+    eval $CMD
+}
+
+function push(){
+
+    echo -e "\nstarting push [${image}] ..."
+    echo "=============================================================="
+    docker push ${image}
+
+    echo -e "\nstarting push [${repo}:latest] ..."
+    echo "=============================================================="
+    docker tag ${image} ${repo}:latest
+    docker push ${repo}:latest
+}
+
+
+case "$1" in
+    "push")
+        build
+        push
+        ;;
+    "")
+        build
+        ;;
+    *)
+        cat <<EOF
+usage:
+    ./build.sh             # build only
+    ./build.sh push        # build and push
+EOF
+    exit 1
+        ;;
+esac
+
+
+
+echo -e "\n=============================================================="
+echo "Done!"
